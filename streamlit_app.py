1816edf534624af3e17d20490a9f3f4750526038
JoeB1989 <w.bellinger.89@gmail.com>
2025-12-17 16:11:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        update fruit_options
-set search_on = 'Raspberry'
-WHERE FRUIT_NAME= 'Raspberries';
+session.sql("""
+    UPDATE fruit_options
+    SET search_on = 'Raspberry'
+    WHERE FRUIT_NAME = 'Raspberries'
+""").collect()
       
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
