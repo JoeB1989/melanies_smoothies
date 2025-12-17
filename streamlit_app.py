4f19eace0c9f71e909629f780970f258b981e994
JoeB1989 <w.bellinger.89@gmail.com>
2025-12-17 15:05:38 -0500

    Update streamlit_app.py
    Updated below and made sure that indentation was good.
    
    if ingredients_list:
        ingredients_string = ''
    
        for fruit_chosen in ingredients_list:
            ingredients_string += fruit_chosen + ' '
            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,13 +31,12 @@ ingredients_list= st.multiselect(
     )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text (ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -56,5 +55,4 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
